7cbd745213b4f204c6f73d6eec81dd205b1e964e
Sawix <13964975+Sawix@users.noreply.github.com>
2021-09-13 01:53:17 -0400

    Fix padding diff

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ function Home() {
   return (
     <section className="py-5">
       <h4 className="mb-4 __text-yellow">Brand Identity</h4>
-      <div className="has-filter">
+      <div className="has-filter py-3">
         <img src="images/filters/filter2.png" className="img-filter" alt="" />
         <img src="images/uxishdesign-cover-1920x1080.png" className="img-fluid" alt="" />
       </div>
