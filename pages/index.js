c5419bb50170842d20446533d65d66a0f198492b
Sawix <13964975+Sawix@users.noreply.github.com>
2021-09-13 01:56:21 -0400

    Padding fix (again)

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,11 @@ function Home() {
   return (
     <section className="py-5">
       <h4 className="mb-4 __text-yellow">Brand Identity</h4>
-      <div className="has-filter py-3">
-        <img src="images/filters/filter2.png" className="img-filter" alt="" />
-        <img src="images/uxishdesign-cover-1920x1080.png" className="img-fluid" alt="" />
+      <div className="py-3">
+        <div className="has-filter">
+          <img src="images/filters/filter2.png" className="img-filter" alt="" />
+          <img src="images/uxishdesign-cover-1920x1080.png" className="img-fluid" alt="" />
+        </div>
       </div>
     </section>
   );
